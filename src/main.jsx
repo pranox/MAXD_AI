303a3c00a54c51f0c4947897f9bbb83d0336128e
pranoxv <pranoxv>
2024-08-06 22:22:18 +0530

    initial

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import './index.css' 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Homepage from './routes/homepage/Homepage';
+import DashboardPage from './routes/dashboardPage/DashboardPage';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+       <Homepage/>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardPage/>,
+    children:[
+      {path:"/dashboard/chats/:id",element:<ChatPage/>}
+    ]
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
